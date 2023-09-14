59a29f34168aa2848e2a69ffe0f68ad6bb41c439
ekilito <2856509748@qq.com>
2023-09-14 22:20:45 +0800

    任务通知列表

---
+++ apis/message.js
@@ -0,0 +1,14 @@
+import { uniFetch } from './uni-fetch.js'
+
+export default {
+  /**
+   * 消息列表
+   * @property {string} contentType - 消息类型
+   * @property {string} page - 消息数据对应的页码
+   * @property {string} pageSize - 每页包含消息数据的条数
+   */
+  list(contentType, page = 1, pageSize = 10) {
+    if (!contentType) return
+    return uniFetch.get('/driver/messages/page', { contentType, page, pageSize })
+  },
+}
--- pages/message/components/notify.vue
+++ pages/message/components/notify.vue
@@ -1,8 +1,32 @@
-<script>
-  export default {
-    options: {
-      // virtualHost: true,
-    },
+<script setup>
+  import { ref, onMounted } from 'vue'
+  // 引入messageAPI
+  import messageAPI from '@/apis/message.js'
+
+  // 任务列表数据 任务通知列表
+  const notifyList = ref([])
+  // 当前列表是否为空列表
+  const isEmpty = ref(false)
+  // 下一页页码
+  //const nextPage = ref(1)
+  // 每页包含数据条数
+  //const pageSize = ref(5)
+
+  // 生命周期
+  onMounted(() => {
+    getNotifyList()
+  })
+
+  async function getNotifyList(page = 1, pageSize = 10) {
+    // 调用接口
+    const { code, data } = await messageAPI.list(201, page, pageSize)
+    // console.log(res)
+    // 检测接口是否调用成功
+    if (code !== 200) return uni.utils.toast('获取通知失败，稍后重试!')
+    // 渲染数据
+    notifyList.value = data.items || []
+    // 是否为空列表
+    isEmpty.value = notifyList.value.length === 0
   }
 </script>
 <template>
@@ -12,67 +36,20 @@
         <text class="iconfont icon-clear"></text>
         全部已读
       </view>
-      <uni-card :border="false" :is-shadow="false">
-        <view class="brief">运输车辆：京A 123456 运输路线：北京—广州</view>
+      <uni-card v-for="notify in notifyList" :key="notify.id" :border="false" :is-shadow="false">
+        <view class="brief">{{ notify.content }}</view>
         <view class="extra">
-          <text class="time">2022.05.04 13:00</text>
-          <navigator
-            hover-class="none"
-            class="link"
-            url="/subpkg_message/content/index"
+          <text class="time">{{ notify.created }}</text>
+          <navigator hover-class="none" class="link" :url="`/subpkg_task/detail/index?id=${notify.id}`"
             >查看详情</navigator
           >
         </view>
         <template v-slot:title>
-          <view class="title unread">您有新的运输任务</view>
+          <view :class="{ unread: !notify.isRead }" class="title">您有新的运输任务</view>
         </template>
       </uni-card>
-      <uni-card :border="false" :is-shadow="false">
-        <view class="brief">运输车辆：京A 123456 运输路线：北京—广州</view>
-        <view class="extra">
-          <text class="time">2022.05.04 13:00</text>
-          <navigator
-            hover-class="none"
-            class="link"
-            url="/subpkg_message/content/index"
-            >查看详情</navigator
-          >
-        </view>
-        <template v-slot:title>
-          <view class="title">您有新的运输任务</view>
-        </template>
-      </uni-card>
-      <uni-card :border="false" :is-shadow="false">
-        <view class="brief">运输车辆：京A 123456 运输路线：北京—广州</view>
-        <view class="extra">
-          <text class="time">2022.05.04 13:00</text>
-          <navigator
-            hover-class="none"
-            class="link"
-            url="/subpkg_message/content/index"
-            >查看详情</navigator
-          >
-        </view>
-        <template v-slot:title>
-          <view class="title">您有新的运输任务</view>
-        </template>
-      </uni-card>
-      <uni-card :border="false" :is-shadow="false">
-        <view class="brief">运输车辆：京A 123456 运输路线：北京—广州</view>
-        <view class="extra">
-          <text class="time">2022.05.04 13:00</text>
-          <navigator
-            hover-class="none"
-            class="link"
-            url="/subpkg_message/content/index"
-            >查看详情</navigator
-          >
-        </view>
-        <template v-slot:title>
-          <view class="title">您有新的运输任务</view>
-        </template>
-      </uni-card>
-      <view v-if="false" class="message-blank">暂无消息</view>
+
+      <view v-if="isEmpty" class="message-blank">暂无消息</view>
     </view>
   </scroll-view>
 </template>
