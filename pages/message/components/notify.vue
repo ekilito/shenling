dd770e021111ef5f40760c20b945f4a3c01452c3
ekilito <2856509748@qq.com>
2023-09-15 14:07:03 +0800

    上拉分页

--- pages/message/components/notify.vue
+++ pages/message/components/notify.vue
@@ -7,30 +7,50 @@
   const notifyList = ref([])
   // 当前列表是否为空列表
   const isEmpty = ref(false)
-  // 下一页页码
-  //const nextPage = ref(1)
+  // 下一页页码 定义页码来记录请求第几页
+  const nextPage = ref(1)
   // 每页包含数据条数
-  //const pageSize = ref(5)
+  const pageSize = ref(5)
+  // 是否还有更多数据
+  const hasMore = ref(true)
 
   // 生命周期
   onMounted(() => {
     getNotifyList()
   })
 
-  async function getNotifyList(page = 1, pageSize = 10) {
+  // 上拉分页
+  function onScrollToLower() {
+    console.log('到底了...开始发送请求获取数据')
+    // 如果没有更多数据了 不获取下一页
+    if (!hasMore.value) return uni.utils.toast('没有更多数据啦')
+    // 获取下一页数据
+    getNotifyList(nextPage.value)
+  }
+
+  async function getNotifyList(page = 1, pageSize = 5) {
     // 调用接口
     const { code, data } = await messageAPI.list(201, page, pageSize)
     // console.log(res)
     // 检测接口是否调用成功
     if (code !== 200) return uni.utils.toast('获取通知失败，稍后重试!')
-    // 渲染数据
-    notifyList.value = data.items || []
+    // 渲染数据  合并数据
+    notifyList.value = [...notifyList.value, ...data.items]
+    // 更新下一页页码
+    nextPage.value = ++data.page
+    // 是否还有更多数据
+    // 如果下一页的页码 `nextPage` 小于等于总页码 `data.page` 时，表明还有更多的数据，否则没有更多数据了
+    hasMore.value = nextPage.value <= data.pages
     // 是否为空列表
     isEmpty.value = notifyList.value.length === 0
   }
 </script>
 <template>
-  <scroll-view class="scroll-view" refresher-enabled scroll-y>
+  <!-- scrolltolower 事件监听页面是否滚动到达底部
+   scroll-y 垂直滚动
+   refresher-enabled 下拉刷新
+   -->
+  <scroll-view @scrolltolower="onScrollToLower" class="scroll-view" refresher-enabled scroll-y>
     <view class="scroll-view-wrapper">
       <view class="message-action">
         <text class="iconfont icon-clear"></text>
